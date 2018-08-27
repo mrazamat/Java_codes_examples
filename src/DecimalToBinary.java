fb10823aca50d13a53ac7194c3057232c8cbcb91
mrazamat <azamat00189@mail.ru>
2018-08-27 18:14:43 +0500

    decimal to octal conversion

---
+++ src/DecimalToBinary.java
@@ -0,0 +1,6 @@
+public class DecimalToBinary {
+    public static void main(String[] args) {
+        System.out.println("Binary representation of 100: " + Integer.toBinaryString(100));
+        System.out.println("Binary representation of 28: " + Integer.toBinaryString(28));
+    }
+}
---
+++ src/DecimalToOctal.java
@@ -0,0 +1,5 @@
+public class DecimalToOctal {
+    public static void main(String[] args) {
+        System.out.println("decimal to octal 123: " + Integer.toOctalString(123));
+    }
+}
