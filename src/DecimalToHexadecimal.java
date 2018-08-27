0f3b7aa3f30ffdc87e34dcafee415a6acc09bfea
mrazamat <azamat00189@mail.ru>
2018-08-27 12:21:19 +0500

    Decimal to hexadecimal Using toHexString() method

---
+++ src/DecimalToHexadecimal.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class DecimalToHexadecimal {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a decimal number: ");
+        int num = input.nextInt();
+
+        String str = Integer.toHexString(num);
+        System.out.println("Method 1: Decimal to hexadecimal: " + str);
+
+    }
+}
