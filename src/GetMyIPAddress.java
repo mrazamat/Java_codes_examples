a38d9dfae332c886331725d6bc2a73c37290838d
mrazamat <azamat00189@mail.ru>
2018-08-28 09:11:08 +0500

    Java program to get IP address

---
+++ src/GetMyIPAddress.java
@@ -0,0 +1,9 @@
+import java.net.InetAddress;
+
+
+public class GetMyIPAddress {
+    public static void main(String[] args) throws Exception {
+        InetAddress myIP = InetAddress.getLocalHost();
+        System.out.println("My IP Address is: " + myIP.getHostAddress());
+    }
+}
