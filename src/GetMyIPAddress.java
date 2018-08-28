d237db3a6a00e980895177ccca86f9f7086fa29b
mrazamat <azamat00189@mail.ru>
2018-08-28 09:14:39 +0500

    Java program to get IP address added method 2

--- src/GetMyIPAddress.java
+++ src/GetMyIPAddress.java
@@ -1,9 +1,20 @@
+import java.net.DatagramSocket;
 import java.net.InetAddress;
 
 
 public class GetMyIPAddress {
     public static void main(String[] args) throws Exception {
+        // Method 1;
         InetAddress myIP = InetAddress.getLocalHost();
         System.out.println("My IP Address is: " + myIP.getHostAddress());
+
+        // Method 2;
+        try(final DatagramSocket socket = new DatagramSocket()){
+            socket.connect(InetAddress.getByName("8.8.8.8"), 10002);
+            String ip = socket.getLocalAddress().getHostAddress();
+            System.out.println(ip);
+        }
+
+
     }
 }
