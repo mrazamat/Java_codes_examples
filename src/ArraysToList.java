a0ef9a94ebaa9b8b7d1f896c540bf20853da7c32
mrazamat <azamat00189@mail.ru>
2018-08-30 08:41:57 +0500

    How to convert Array to List in java?

---
+++ src/ArraysToList.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class ArraysToList {
+    public static void main(String[] args) {
+        String[] strArr = {"JAVA","C++","PERL","PHP","PLAY" };
+        List<String> strList = Arrays.asList(strArr);
+        System.out.println("Created List Size: " + strList.size());
+        System.out.println(strList);
+    }
+}
