4777b1c1961a466270053c7017cc5ed81e79e9b6
mrazamat <azamat00189@mail.ru>
2018-09-04 11:20:29 +0500

    Java Singleton Class Example Using Private Constructor

---
+++ src/MySingleTon.java
@@ -0,0 +1,20 @@
+public class MySingleTon {
+    private static MySingleTon myObj;
+    private MySingleTon(){
+
+    }
+    public static MySingleTon getInstance(){
+        if(myObj == null)
+            myObj = new MySingleTon();
+        return myObj;
+    }
+
+    public void getSomeThing(){
+        System.out.println("I am here....");
+    }
+
+    public static void main(String[] args) {
+        MySingleTon st = MySingleTon.getInstance();
+        st.getSomeThing();
+    }
+}
