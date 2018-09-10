8a167f9d0aef9c9da06684e6a551ec3a9004a696
mrazamat <azamat00189@mail.ru>
2018-09-10 16:11:03 +0500

    How to iterate through Hashtable?

--- src/uz/coder/collections/Hashtable/MyHashTableRead.java
+++ src/uz/coder/collections/Hashtable/MyHashTableRead.java
@@ -1,4 +1,19 @@
 package uz.coder.collections.Hashtable;
 
+import java.util.Hashtable;
+import java.util.Set;
+
 public class MyHashTableRead {
+    public static void main(String[] args) {
+        Hashtable<String, String> hm = new Hashtable<>();
+        hm.put("first", "FIRST INSERTED");
+        hm.put("second", "SECOND INSERTED");
+        hm.put("third","THIRD INSERTED");
+        System.out.println(hm);
+
+        Set<String> keys = hm.keySet();
+        for (String key:keys) {
+            System.out.println("Value of " + key + " is: " + hm.get(key));
+        }
+    }
 }
