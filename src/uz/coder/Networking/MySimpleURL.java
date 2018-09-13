5d0f46d37ddc51992a8c72665cb9b39f5aa459dd
mrazamat <azamat00189@mail.ru>
2018-09-13 11:37:17 +0500

    Java Default URL Object Example

---
+++ src/uz/coder/Networking/MySimpleURL.java
@@ -0,0 +1,52 @@
+// Java Default URL Object Example
+// Class URL represents a Uniform Resource Locator, a pointer to a "resource" on the World Wide Web.
+// A resource can be something as simple as a file or a directory,
+// or it can be a reference to a more complicated object, such as a query to a database or to a search engine.
+// java.net.URL creates a URL object from the String representation.
+
+package uz.coder.Networking;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+public class MySimpleURL {
+    public static void main(String[] args) {
+        try{
+            String myURL = "https://google.com";
+            URL url = new URL(myURL);
+            System.out.println(url.getAuthority());
+            System.out.println(url.getDefaultPort());
+            System.out.println(url.getProtocol());
+            System.out.println(url.getUserInfo());
+            System.out.println(url.toURI());
+            System.out.println(url.getHost());
+            System.out.println(url.toString());
+        } catch(MalformedURLException ex){
+            ex.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
