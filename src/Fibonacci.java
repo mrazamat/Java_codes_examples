c3f35bbea9cf5ee6c234542cb23033f8e09e72d6
mrazamat <azamat00189@mail.ru>
2018-09-26 09:22:38 +0500

    Java program to compute n-th Fibonacci number where n may be large.

---
+++ src/Fibonacci.java
@@ -0,0 +1,26 @@
+// Java program to compute n-th Fibonacci
+// number where n may be large.
+
+import java.math.BigInteger;
+
+public class Fibonacci {
+    // Returns n-th Fibonacci number
+    static BigInteger fib(int n) {
+        BigInteger a = BigInteger.valueOf(0);
+        BigInteger b = BigInteger.valueOf(1);
+        BigInteger c = BigInteger.valueOf(1);
+        for (int j = 2; j <= n; j++) {
+            c = a.add(b);
+            a = b;
+            b = c;
+        }
+
+        return (a);
+    }
+
+    public static void main(String[] args) {
+        int n = 1000;
+        System.out.println("Fibonacci of " + n +
+                "th term" + " " + "is" + " " + fib(n));
+    }
+}
