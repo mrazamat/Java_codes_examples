daf1e6982bf477ea1f92c4aecb44a3a0c49c2f92
Razakov Azamat <azamat00189@mail.ru>
2018-11-17 20:58:48 +0500

    changed any codes

---
+++ src/uz/coder/email/SendFileEmail.java
@@ -0,0 +1,58 @@
+package uz.coder.email;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class SendFileEmail {
+    public static void main(String [] args) {
+        // Recipient's email ID needs to be mentioned.
+        String to = "azamat@coder.uz";
+
+        // Sender's email ID needs to be mentioned
+        String from = "azamat00189@mail.ru";
+
+        // Assuming you are sending email from localhost
+        String host = "mail.smtp.ru";
+
+        // Get system properties
+        Properties properties = System.getProperties();
+
+        // Setup mail server
+        properties.setProperty("mail.smtp.host", host);
+        properties.setProperty("mail.smtp.port", "465");
+        properties.setProperty("mail.smtp.auth", "true");
+        properties.setProperty("mail.user", "test@mail.ru");
+        properties.setProperty("mail.password", "password");
+
+        // Get the default Session object.
+        Session session = Session.getDefaultInstance(properties);
+
+        try {
+            // Create a default MimeMessage object.
+            MimeMessage message = new MimeMessage(session);
+
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress(from));
+
+            // Set To: header field of the header.
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+            // Set Subject: header field
+            message.setSubject("This is the Subject Line!");
+
+            // Now set the actual message
+            message.setText("This is actual message");
+
+            // Send message
+            Transport.send(message);
+            System.out.println("Sent message successfully....");
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+    }
+}
