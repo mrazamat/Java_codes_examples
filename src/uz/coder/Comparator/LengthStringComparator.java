3cb5a588e5294252d287c3f9d0b77f35d5b6650a
Azamat <azamat00189@mail.ru>
2018-12-22 15:44:30 +0500

    Logger test

---
+++ src/uz/coder/Comparator/LengthStringComparator.java
@@ -0,0 +1,10 @@
+package uz.coder.Comparator;
+
+import java.util.Comparator;
+
+public class LengthStringComparator implements Comparator {
+    @Override
+    public int compare(Object o1, Object o2) {
+        return 0;
+    }
+}
